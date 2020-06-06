51613aec1690de02baeae30ab7bd24fe33ca62e3
Ganesh Kulkarni <53935421+ganeshkulkarni98@users.noreply.github.com>
2020-06-07 00:08:52 +0530

    Update run.py

--- run.py
+++ run.py
@@ -58,27 +58,23 @@ def convert_labelbox_json(data_folder_path, json_file_dir):
     for x in tqdm(data['annotations'], desc='Annotations'):
         i = file_id.index(x['image_id'])  # image index
         label_name = Path(file_name[i]).stem + '.txt'
-
-        # The Labelbox bounding box format is [top left x, top left y, width, height]
+        
+        # The Labelbox bounding box format is [top left x, top left y, width, height
+        if x['bbox'] ==[]:
+          x['bbox'] = [0,0,0,0]
+          
         box = np.array(x['bbox'], dtype=np.float64)
         box[:2] += box[2:] / 2  # xy top-left corner to center
-        if width[i] == 0:
-            box[0] = 1
-            box[2] = 1    # normalize x
-        else:
-            box[[0, 2]] /= width[i]  # normalize x
-        if height[i] == 0:   
-            box[1] = 1
-            box[3] = 1       # normalize y
-        else:
-            box[[1, 3]] /= height[i]  # normalize y
-            
-        if (box[2] > 0.) and (box[3] > 0.):  # if w > 0 and h > 0
-            
-            
-            with open(os.path.join(labels_folder, label_name), 'a') as file:
-                file.write('%g %.6f %.6f %.6f %.6f\n' % (x['category_id'] , *box))
 
+        if int(box[2]) != 0:
+          box[[0, 2]] /= width[i]  # normalize x
+          box[[1, 3]] /= height[i]  # normalize y
+            
+        #if (box[2] > 0.) and (box[3] > 0.):  # if w > 0 and h > 0
+            
+            
+        with open(os.path.join(labels_folder, label_name), 'a') as file:
+            file.write('%g %.6f %.6f %.6f %.6f\n' % (x['category_id'] , *box))
     # Split data into train, test, and validate files
     split_files(name, file_name)                                 #split whole data 90% for training and 10% for validation
     print('Done. Output saved to %s' % (data_folder_path))
